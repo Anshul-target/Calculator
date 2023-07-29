0e9a23c6ab83b8dc4408c01058186a4fdc12b7ab
Anshul <ayushyadav6a@gmail.com>
2023-07-29 13:50:33 +0530

    New Project

--- src/code/handleClick.js
+++
@@ -1,5 +0,0 @@
-import { useState } from "react"
-export const HandleClick = () => {
-    const [number, setNumber] = useState("")
-    setNumber = event.target.textContent
-}
--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,41 @@
 // import { HandleClick } from "../code/handleClick";
 import { useState } from "react"
+
 export const Calculator = () => {
 
-    const [number, setNumber] = useState("")
+    const [number, setNumber] = useState("");
+    const [result, setResult] = useState("")
     const handleClick = ({ target }) => {
+        setNumber(number + target.textContent);
+        // showResult();
+    }
+    const handleAc = () => {
+        setNumber("")
+    }
+    const handleClear = () => {
 
-        setNumber(target.textContent)
+        setNumber(number.substring(0, (number.length) - 1))
+    }
+
+    const handleOperation = ({ target }) => {
+        const findSymbol = number.charAt(number.length - 1)
+        if (findSymbol == "%" || findSymbol == "*" || findSymbol == "/" || findSymbol == "-" || findSymbol == "+") {
+            return
+        }
+        setNumber(number + target.textContent)
+        showResult();
+    }
+
+    const showResult = () => {
+
+        const findSymbol = number.charAt(number.length - 1)
+        if (findSymbol == "%" || findSymbol == "*" || findSymbol == "/" || findSymbol == "-" || findSymbol == "+") {
+            setNumber("Syntax Error");
+            setResult("");
+            return;
+        }
+
+        setResult((eval(number)).toString());
     }
 
     return (
@@ -15,33 +45,33 @@ export const Calculator = () => {
             <div className="main-container">
 
                 <div className="item box">
-                    <div className="output-box boxes">23</div>
-                    <div className="input-box boxes">32</div>
+                    <div className="output-box boxes">{number}</div>
+                    <div className="input-box boxes">{result}</div>
                 </div>
                 <button type="button" className="items small-font">&radic;</button>
                 <button type="button" className="items small-font">   &pi;</button>
                 <button type="button" className="items small-font">^</button>
                 <button type="button" className="items small-font">!</button>
-                <button type="button" className="items" style={{ backgroundColor: "#0F5324" }}>AC</button>
-                <button type="button" className="items" style={{ backgroundColor: "#004A77" }}>( )</button>
-                <button type="button" className="items" style={{ backgroundColor: "#004A77" }}>% </button>
-                <button type="button" className="items" style={{ backgroundColor: "#004A77" }}>/</button>
+                <button type="button" className="items" style={{ backgroundColor: "#0F5324" }} onClick={handleAc}>AC</button>
+                <button type="button" className="items" style={{ backgroundColor: "#004A77" }} onClick={handleOperation}>( )</button>
+                <button type="button" className="items" style={{ backgroundColor: "#004A77" }} onClick={handleOperation}>% </button>
+                <button type="button" className="items" style={{ backgroundColor: "#004A77" }} onClick={handleOperation}>/</button>
                 <button type="button" className="items" onClick={handleClick}>7</button>
                 <button type="button" className="items" onClick={handleClick}>8</button>
                 <button type="button" className="items" onClick={handleClick}>9</button>
-                <button type="button" className="items" style={{ backgroundColor: "#004A77" }}>*</button>
+                <button type="button" className="items" style={{ backgroundColor: "#004A77" }} onClick={handleOperation}>*</button>
                 <button type="button" className="items" onClick={handleClick}>4</button>
                 <button type="button" className="items" onClick={handleClick}>5</button>
                 <button type="button" className="items" onClick={handleClick}>6</button>
-                <button type="button" className="items" style={{ backgroundColor: "#004A77" }}>-</button>
+                <button type="button" className="items" style={{ backgroundColor: "#004A77" }} onClick={handleOperation}>-</button>
                 <button type="button" className="items" onClick={handleClick}>1</button>
                 <button type="button" className="items" onClick={handleClick}>2</button>
                 <button type="button" className="items" onClick={handleClick}>3</button>
-                <button type="button" className="items" style={{ backgroundColor: "#004A77" }}>+</button>
+                <button type="button" className="items" style={{ backgroundColor: "#004A77" }} onClick={handleOperation}>+</button>
                 <button type="button" className="items" onClick={handleClick}>0</button>
                 <button type="button" className="items" onClick={handleClick}>.</button>
-                <button type="button" className="items">  &times;</button>
-                <button type="button" className="items" style={{ backgroundColor: "#004A77" }}>=</button>
+                <button type="button" className="items" onClick={handleClear}>  &times;</button>
+                <button type="button" className="items" style={{ backgroundColor: "#004A77" }} onClick={showResult} >=</button>
 
 
             </div>
