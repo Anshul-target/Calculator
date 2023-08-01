b3dafa8d1dfe89409150e93d8f0847f62ca8ecdd
Anshul <ayushyadav6a@gmail.com>
2023-08-01 17:19:44 +0530

    New Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 // import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Calculator } from './components/home';
 
 function App() {
   return (
-    <Calculator />
+    <Routes>
+      <Route path='/calculator' element={<Calculator />} />
+
+    </Routes>
   )
 }
 
